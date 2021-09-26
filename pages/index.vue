87b132a268644fb3b10a9aed786708ebcef0fc88
cuongdm <cuongdm@flextech.vn>
2021-09-26 22:14:43 +0700

    feat

---
+++ pages/index.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="bg-bantinh">
+    <div class="text-center text-white result">
+      {{ count }}
+    </div>
+    <div class="soroban flex">
+      <div v-for="i in hanghat" :key="i" class="hanghat" :class="`hanghat-${14-i}`">
+        <div :id="`hat-five-${i}`" class="hat-five" @click="runHatFive($event)" />
+        <div :id="`hat-one-${i}`" class="hat-one">
+          <div :id="`one-1-${i}`" class="one-1" @click="runHatOne($event)" />
+          <div :id="`one-2-${i}`" class="one-2" @click="runHatOne($event)" />
+          <div :id="`one-3-${i}`" class="one-3" @click="runHatOne($event)" />
+          <div :id="`one-4-${i}`" class="one-4" @click="runHatOne($event)" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { calculate, calculateFive } from '~/utils/calculate'
+
+function addClass (element, className) {
+  element.classList.add(className)
+};
+function removeClass (element, className) {
+  element.classList.remove(className)
+};
+export default {
+  data () {
+    return {
+      count: 0,
+      hanghat: 13
+    }
+  },
+  methods: {
+    hasClass (element, className) {
+      do {
+        if (element.classList && element.classList.contains(className)) {
+          return true
+        }
+        element = element.parentNode
+      } while (element)
+      return false
+    },
+    runHatFive (event) {
+      const element = document.getElementById(event.currentTarget.id)
+      const nameClassParent = element.parentNode.classList[1]
+      if (this.hasClass(element, 'active-hat-five') === false) {
+        addClass(element, 'active-hat-five')
+        this.count += calculateFive(nameClassParent)
+      } else {
+        removeClass(element, 'active-hat-five')
+        this.count -= calculateFive(nameClassParent)
+      }
+    },
+    runHatOne (event) {
+      const element = document.getElementById(event.currentTarget.id)
+      const elementParent = document.getElementById(element.parentNode.id)
+      const nameClassParent = elementParent.parentNode.classList[1]
+      switch (event.toElement.classList[0]) {
+        case 'one-1':
+          if (this.hasClass(elementParent, 'active-1') === false) {
+            addClass(elementParent, 'active-1')
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-4') === true) {
+            removeClass(elementParent, 'active-1')
+            removeClass(elementParent, 'active-2')
+            removeClass(elementParent, 'active-3')
+            removeClass(elementParent, 'active-4')
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-3') === true) {
+            removeClass(elementParent, 'active-1')
+            removeClass(elementParent, 'active-2')
+            removeClass(elementParent, 'active-3')
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-2') === true) {
+            removeClass(elementParent, 'active-1')
+            removeClass(elementParent, 'active-2')
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+          } else {
+            removeClass(elementParent, 'active-1')
+            this.count -= calculate(nameClassParent)
+          }
+          break
+
+        case 'one-2':
+          if (this.hasClass(elementParent, 'active-4') === true) {
+            removeClass(elementParent, 'active-2')
+            removeClass(elementParent, 'active-3')
+            removeClass(elementParent, 'active-4')
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-3') === true) {
+            removeClass(elementParent, 'active-2')
+            removeClass(elementParent, 'active-3')
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-2') === true) {
+            removeClass(elementParent, 'active-2')
+            this.count -= calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-1') === false) {
+            addClass(elementParent, 'active-1')
+            addClass(elementParent, 'active-2')
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-1') === true) {
+            addClass(elementParent, 'active-2')
+            this.count += calculate(nameClassParent)
+          }
+          break
+        case 'one-3':
+          if (this.hasClass(elementParent, 'active-4') === true) {
+            removeClass(elementParent, 'active-3')
+            removeClass(elementParent, 'active-4')
+            this.count -= calculate(nameClassParent)
+            this.count -= calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-2') === true && this.hasClass(elementParent, 'active-3') === false) {
+            addClass(elementParent, 'active-3')
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-1') === false) {
+            addClass(elementParent, 'active-3')
+            addClass(elementParent, 'active-1')
+            addClass(elementParent, 'active-2')
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-2') === false && this.hasClass(elementParent, 'active-1') === true) {
+            addClass(elementParent, 'active-2')
+            addClass(elementParent, 'active-3')
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-2') === true && this.hasClass(elementParent, 'active-3') === false) {
+            addClass(elementParent, 'active-3')
+            this.count += calculate(nameClassParent)
+          } else {
+            removeClass(elementParent, 'active-3')
+            this.count -= calculate(nameClassParent)
+          }
+          break
+        case 'one-4':
+          if (this.hasClass(elementParent, 'active-1') === false) {
+            addClass(elementParent, 'active-1')
+            addClass(elementParent, 'active-2')
+            addClass(elementParent, 'active-3')
+            addClass(elementParent, 'active-4')
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-2') === false) {
+            addClass(elementParent, 'active-2')
+            addClass(elementParent, 'active-3')
+            addClass(elementParent, 'active-4')
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-3') === false) {
+            addClass(elementParent, 'active-3')
+            addClass(elementParent, 'active-4')
+            this.count += calculate(nameClassParent)
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-4') === false) {
+            addClass(elementParent, 'active-4')
+            this.count += calculate(nameClassParent)
+          } else if (this.hasClass(elementParent, 'active-3') === true) {
+            removeClass(elementParent, 'active-4')
+            this.count -= calculate(nameClassParent)
+          }
+          break
+
+        default:
+          break
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ utils/calculate.js
@@ -0,0 +1,96 @@
+export const calculate = (className) => {
+  let count = null
+  switch (className) {
+    case 'hanghat-1':
+      count = 1
+      break
+    case 'hanghat-2':
+      count = 10
+      break
+    case 'hanghat-3':
+      count = 100
+      break
+    case 'hanghat-4':
+      count = 1000
+      break
+    case 'hanghat-5':
+      count = 10000
+      break
+    case 'hanghat-6':
+      count = 100000
+      break
+    case 'hanghat-7':
+      count = 1000000
+      break
+    case 'hanghat-8':
+      count = 10000000
+      break
+    case 'hanghat-9':
+      count = 100000000
+      break
+    case 'hanghat-10':
+      count = 1000000000
+      break
+    case 'hanghat-11':
+      count = 10000000000
+      break
+    case 'hanghat-12':
+      count = 100000000000
+      break
+    case 'hanghat-13':
+      count = 1000000000000
+      break
+    default:
+      count = 1
+      break
+  }
+  return count
+}
+export const calculateFive = (className) => {
+  let count = null
+  switch (className) {
+    case 'hanghat-1':
+      count = 5
+      break
+    case 'hanghat-2':
+      count = 50
+      break
+    case 'hanghat-3':
+      count = 500
+      break
+    case 'hanghat-4':
+      count = 5000
+      break
+    case 'hanghat-5':
+      count = 50000
+      break
+    case 'hanghat-6':
+      count = 500000
+      break
+    case 'hanghat-7':
+      count = 5000000
+      break
+    case 'hanghat-8':
+      count = 50000000
+      break
+    case 'hanghat-9':
+      count = 500000000
+      break
+    case 'hanghat-10':
+      count = 5000000000
+      break
+    case 'hanghat-11':
+      count = 50000000000
+      break
+    case 'hanghat-12':
+      count = 500000000000
+      break
+    case 'hanghat-13':
+      count = 5000000000000
+      break
+    default:
+      count = 0
+      break
+  }
+  return count
+}
